8ccda35e6f399cc71b07f00af5cd89d1026c4e18
Giulio Como <s328199@studenti.polito.it>
2024-10-27 16:51:37 +0100

    test db configuration

--- backend/db/db.mjs
+++ backend/db/db.mjs
@@ -1,9 +1,10 @@
 /** DB access module **/
 
 import sqlite3 from "sqlite3";
+import { dbFilePath } from "./path.mjs";
 
 // Opening the database
-const db = new sqlite3.Database('./db/db.db', (err) => {
+const db = new sqlite3.Database(dbFilePath, (err) => {
     if (err) throw err;
     console.log('Connected to the SQLite database.');
 
