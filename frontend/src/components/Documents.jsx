a9b14e89c2e2c705287b0468a83dd422e656dd77
ALESSANDRO GENCO <s324502@studenti.polito.it>
2024-11-06 21:50:56 +0100

    Update Documents.jsx

--- frontend/src/components/Documents.jsx
+++ frontend/src/components/Documents.jsx
@@ -92,7 +92,6 @@ function Documents({ show, handleClose }) {
       setDocuments((prevDocuments) => prevDocuments.filter((doc) => doc.id !== selectedDocument));
       setSelectedDocument(null);
       setMessage('Document deleted successfully!');
-    window.location.reload(); 
     } catch (error) {
       console.error('Error deleting document:', error);
       setMessage('Error deleting document.');
