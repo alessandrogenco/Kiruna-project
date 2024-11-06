e3ffef6364d8a7b97f8b65536036ebb8b3680f6f
Proglio Davide <s272199@studenti.polito.it>
2024-11-06 18:26:30 +0100

    responsive fix

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -20,14 +20,14 @@ function HomePage(props) {
       <div className="homepage">
         <div className="welcome-text">
           <h1 className="text-shadow-basic" style={{ textAlign: 'left' }}>KirunaExplorer</h1>
-          <p className="text-shadow-basic" style={{ textAlign: 'left', paddingBottom: '3em' }}>
+          <p className="text-shadow-basic" style={{ textAlign: 'left', paddingBottom: '1em' }}>
             {isMobile
               ? "Explore Kiruna, a unique Swedish city that tells a story of transformation through its architecture. Join us to understand how the mining industry has shaped the urban landscape and identity of Kiruna."
               : "Discover Kiruna, a unique Swedish city that tells a story of transformation and innovation through its architecture. Due to mining development, Kiruna has undergone profound changes over the years, with buildings that reflect its industrial history and resilient spirit. In this guide, you will explore the highlights of the city’s architectural evolution, from its iconic historic buildings to modern sustainable solutions. Join us on this journey to understand how mining has shaped not only the urban landscape but also the identity of Kiruna."
             }
           </p>
           <div className="button-container button-shadow">
-            <button className="btn-grad mt-4" onClick={() => navigate('/explore')}>
+            <button className="btn-grad mt-2" onClick={() => navigate('/explore')}>
               EXPLORE
             </button>
           </div>
