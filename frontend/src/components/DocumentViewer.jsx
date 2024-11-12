f476af8e4b8a1b27d5cf35449767cca49d5e2c8e
niloofarbani <niloofarbani98@gmail.com>
2024-11-12 19:01:46 +0100

    card update

--- frontend/src/components/DocumentViewer.jsx
+++ frontend/src/components/DocumentViewer.jsx
@@ -9,17 +9,14 @@ const DocumentViewer = ({ documentData, onClose }) => {
   return (
     <div className="my-document-viewer-wrapper">
       <div className="document-viewer">
-        {/* Close button always available at the top right */}
+        
         <button className="close-button" onClick={onClose}>&times;</button>
-
         {!viewDescription ? (
           <div className="document-details">
             <h3>{documentData.title}</h3>
             <p><strong>Stakeholder:</strong> {documentData.stakeholder}</p>
             <p><strong>Issuance date:</strong> {documentData.issuanceDate}</p>
             <p><strong>Type:</strong> {documentData.type}</p>
-
-            {/* Button group for "View Description" and "Close" */}
             <div className="button-group">
               <button onClick={() => setViewDescription(true)}>View Description</button>
             </div>
@@ -29,7 +26,6 @@ const DocumentViewer = ({ documentData, onClose }) => {
             <h3>{documentData.title}</h3>
             <p><strong>Description:</strong> {documentData.description}</p>
 
-            {/* Button group for "Go Back" and "Close" */}
             <div className="button-group">
               <button onClick={() => setViewDescription(false)}>Go Back</button>
             </div>
