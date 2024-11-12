83c9e56d0727c0ced6161f1962a2a2bac01f5dd1
Noman Rafiq <nomannrafiq@gmail.com>
2024-11-12 02:26:02 +0100

    originalResource Dao function added

---
+++ backend/dao/fileUpload.mjs
@@ -0,0 +1,25 @@
+import db from '../db/db.mjs';
+
+class fileUpload{
+
+    addOrignalResource(documentId, resource){
+        return new Promise ((resolve, reject) => {
+            const query = `
+                INSERT INTO OriginalResources (documentId, resourceType, resourcePath, description)
+                VALUES (?,?,?,?);`
+
+            const {resourceType, resourcePath, description} = resource;
+
+            db.run(query, [documentId, resourceType, resourcePath, description], function(err) {
+
+                if(err){
+                    console.error('Error uploading file:', err.message);
+                    return reject(new Error('Failed to upload file'));
+                }
+                resolve({resourceId: this.lastID})
+            });
+        })
+    }
+}
+
+export default fileUpload;
