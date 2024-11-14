85be164cac0e67d239574d0e1b2cfd4a7f0ae02d
niloofarbani <niloofarbani98@gmail.com>
2024-11-14 21:27:40 +0100

    remove close button, increase font size and add padding

--- frontend/src/components/ExplorePage.jsx
+++ frontend/src/components/ExplorePage.jsx
@@ -138,12 +138,12 @@ function ExplorePage(props) {
 
           const popup = new mapboxgl.Popup({ 
             offset: 25,
-            closeButton: true, 
+            closeButton: false, 
             })
             .setHTML(`
-              <div style="padding-right: 0.8em; margin-bottom: -0.4em; display: flex; flex-direction: column;">
-                <div>${properties.label}</div>
-                <button id="view-details" style="margin-top: 5px; padding: 5px; background-color: #218838; color: white; border: none; border-radius: 4px; cursor: pointer;">
+              <div style="padding: 5px; margin-bottom: -0.4em; display: flex; flex-direction: column;">
+                 <div style="font-size: 1.2em; font-weight: bold;">${properties.label}</div>
+                <button id="view-details" style="margin-top: 12px; padding: 5px; background-color: #218838; color: white; border: none; border-radius: 4px; cursor: pointer;">
                   View Description
                 </button>
               </div>
