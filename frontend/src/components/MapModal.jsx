a638e1e8e644f6231a5c00758d4c90f2a9f3ae89
Giulio Como <s328199@studenti.polito.it>
2024-11-27 00:00:31 +0100

    updated boundaries on map modal

--- frontend/src/components/MapModal.jsx
+++ frontend/src/components/MapModal.jsx
@@ -3,6 +3,7 @@ import { Modal, Button, ToggleButtonGroup, ToggleButton } from 'react-bootstrap'
 import PropTypes from 'prop-types';
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
+import * as turf from '@turf/turf';
 
 const MapModal = ({ show, handleClose, onLocationSelect }) => {
   const mapContainer = useRef(null);
@@ -10,64 +11,93 @@ const MapModal = ({ show, handleClose, onLocationSelect }) => {
   const marker = useRef(null);
   const [position, setPosition] = useState(null);
   const [mode, setMode] = useState('point');
+  const [geoJsonData, setGeoJsonData] = useState(null);
 
   const MAPBOX_TOKEN = "pk.eyJ1IjoiYWxlc3NhbmRyb2cwOCIsImEiOiJjbTNiZzFwbWEwdnU0MmxzYTdwNWhoY3dpIn0._52AcWROcPOQBr1Yz0toKw";
-
+  
   useEffect(() => {
     if (show && mapContainer.current && !map.current) {
       mapboxgl.accessToken = MAPBOX_TOKEN;
-  
+
       map.current = new mapboxgl.Map({
         container: mapContainer.current,
         style: 'mapbox://styles/mapbox/streets-v11',
         center: [20.25, 67.85], 
-        zoom: 12,
-        maxBounds: [[20.12, 67.82], [20.40, 67.88]], 
+        zoom:11,
+        maxBounds: [[17.8998, 67.3562], [23.2867, 69.0599]],
       });
-  
-      map.current.on('load', () => {
+
+      map.current.on('load', async () => {
         map.current.resize();
+
+        const response = await fetch('/KirunaMunicipality.geojson');
+        const geojson = await response.json();
+        setGeoJsonData(geojson); 
+
+        // Add the GeoJSON file
+        map.current.addSource('kiruna-boundary', {
+          type: 'geojson',
+          data: geojson, 
+        });
+
+        map.current.addLayer({
+          id: 'kiruna-boundary-border',
+          type: 'line',  
+          source: 'kiruna-boundary',  
+          paint: {
+            'line-color': '#007cbf',   
+            'line-width': 3,           
+            'line-opacity': 1,         
+          },
+        });
+        
+        map.current.on('click', (e) => {
+          if (mode === 'point') {
+            const { lng, lat } = e.lngLat;
+
+            if (geoJsonData) {
+              if (!isWithinBounds(lng, lat, geoJsonData)) {
+                alert("Selected coordinates are out of Kiruna Municipality borders!");
+                return;
+              }
+
+              setPosition([lat, lng]);
+
+              if (marker.current) {
+                marker.current.setLngLat([lng, lat]);
+              } else {
+                marker.current = new mapboxgl.Marker({ color: '#007cbf' })
+                  .setLngLat([lng, lat])
+                  .addTo(map.current);
+              }
+            }
+          } else {
+            alert("Area selection mode is under development!");
+          }
+        });
       });
-  
-      map.current.on('click', (e) => {
-        if (mode === 'point'){
-        const { lng, lat } = e.lngLat; 
-  
-        if (lng < 20.1200 || lng > 20.4000 || lat < 67.82 || lat > 67.8800) {
-          alert("Selected coordinates are out of Kiruna Municipality borders!");
-          return;
+
+      return () => {
+        if (map.current) {
+          map.current.remove();
+          map.current = null;
+          marker.current = null;
         }
-  
-        setPosition([lat, lng]);
-  
-        if (marker.current) {
-          marker.current.setLngLat([lng, lat]); 
-        } else {
-          marker.current = new mapboxgl.Marker({ color: '#007cbf' })
-            .setLngLat([lng, lat]) 
-            .addTo(map.current);
-        }
-      } else {
-        alert ("Area selection mode is under development!")
-      }
-      });
-    };
-  
-    if (show && map.current) {
-      setTimeout(() => {
-        map.current.resize();
-      }, 200);
+      };
     }
-  
-    return () => {
-      if (map.current) {
-        map.current.remove();
-        map.current = null;
-        marker.current = null;
-      }
-    };
   }, [show, mode]);
-  
+
+  const isWithinBounds = (lon, lat, geojson) => {
+    const point = turf.point([lon, lat]);  
+
+    const isInside = geojson.features[0].geometry.coordinates.some((polygonCoordinates) => {
+      const polygon = turf.polygon(polygonCoordinates);
+      return turf.booleanPointInPolygon(point, polygon);
+    });
+
+    return isInside;
+  };
+
   const handleSave = () => {
     if (position) {
       onLocationSelect(position);
