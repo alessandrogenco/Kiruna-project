d9d9494515755b4fa93be29dc42ccd390b6f218a
Greta Tumiatti <s332148@studenti.polito.it>
2024-12-02 18:33:23 +0100

    tech debt

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    'process.env': process.env
+export default defineConfig(({ mode }) => {
+  // Carica le variabili di ambiente dal file .env
+  const env = loadEnv(mode, process.cwd(), 'REACT_APP_');
+
+  return {
+    define: {
+      // Definisci le variabili di ambiente per l'uso nel codice
+      'process.env.REACT_APP_MAPBOX_TOKEN': JSON.stringify(env.REACT_APP_MAPBOX_TOKEN),
+      'process.env.REACT_APP_MAPBOX_TOKEN1': JSON.stringify(env.REACT_APP_MAPBOX_TOKEN1),
+    },
+    plugins: [react()],
   }
-})
+});
